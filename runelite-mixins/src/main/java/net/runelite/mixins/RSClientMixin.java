f1bfe1609e8f8b1315333b4d83161f743fbfcd7b
Irmuun <rokahakor@gmail.com>
2021-08-28 21:36:01 -0700

    Update tear collector

--- runelite-mixins/src/main/java/net/runelite/mixins/RSClientMixin.java
+++ runelite-mixins/src/main/java/net/runelite/mixins/RSClientMixin.java
@@ -792,8 +792,8 @@ public abstract class RSClientMixin implements RSClient
 	}
 
 	@SuppressWarnings("InfiniteRecursion")
-	@Copy("method4321")
-	@Replace("method4321")
+//	@Copy("method4321")
+//	@Replace("method4321")
 	private static void copy$menuSorter() {
 		copy$menuSorter();
 
